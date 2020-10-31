22902fa822146ea43ff9ba91d9320e175b5b7775
Godwin Obamina <38545029+Owebiwan@users.noreply.github.com>
2020-10-31 14:57:43 +0100

    Updates

---
+++ precache-manifest.e74bde4e1b0d785ea76f619ea0dd0031.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66a75cc6089aeef276c0603613540411",
+    "url": "/neumorpher/index.html"
+  },
+  {
+    "revision": "67134ba69dde588edf99",
+    "url": "/neumorpher/static/css/main.7ec00d9e.chunk.css"
+  },
+  {
+    "revision": "83dc5063dafff1c2ee5e",
+    "url": "/neumorpher/static/js/2.90e6924f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/neumorpher/static/js/2.90e6924f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67134ba69dde588edf99",
+    "url": "/neumorpher/static/js/main.64251baf.chunk.js"
+  },
+  {
+    "revision": "bb4831bdced5d63ef3f2",
+    "url": "/neumorpher/static/js/runtime-main.c86f6a4b.js"
+  }
+]);
